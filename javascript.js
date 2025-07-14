9390aaacc411e7933b8133dce132d7d67dd8a6ba
arunheera <arunheeraa@gmail.com>
2025-07-14 10:34:34 +0530

    file uploaded

---
+++ javascript.js
@@ -0,0 +1,34 @@
+
+// {
+//     // a=10;
+//     // // b=10;
+//     console.log(a);
+//     // console.log(b);
+     
+    
+//     var a;
+//     // let b;
+
+// }
+
+
+
+
+// {
+//     let a=10;
+//     console.log(a);
+//      let a=20;
+//      console.log(a);
+// }
+
+
+
+
+
+
+{
+    const a=10;
+   console.log(a);
+   a =20;
+   console.log(a);
+}
